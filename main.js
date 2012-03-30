0c8b622025e69bd944e7da8db8baa3b3260c1266
iknowchang <iknowchang@gmail.com>
2012-03-30 11:33:01 +0900

    영문 제목에대해서는 작동하도록 수정

--- main.js
+++ main.js
@@ -1,34 +1,68 @@
-(function () {
-  
-  var
-    // jquery object
-    $castWrap = $('#cast_articles'),
-    
-    SEARCH_DOCUMENT_URL = 'http://news.naver.com/main/search/search.nhn';
-  
-  function init() {
-    overrideNewsClickEvent();
-  }
- 
-  function overrideNewsClickEvent() {
-    $castWrap.delegate('a', 'click', function (e) {
-      moveToEditedPage($(this));
-      e.preventDefault(); 
-    });    
-  }
-  
-  function moveToEditedPage($a) {
-    var title = getTitle($a);
-    console.log(title);
-  }
-  
-  function getTitle($a) {
-    var title = $a.text() || $a.find('img').attr('alt'); // 이미지가 포함된 경우 alt 속성을 가져온다.
-    // 뉴스 서비스에서는 공백을 +로 대체한다.
-    title = title.replace(/\s/g, '+');
-    return title;
-  }
-  
-  init();
-  
+(
+function() {
+	var READYSTATE_UNINITIALIZED = 0;
+	var READYSTATE_LOADING = 1;
+	var READYSTATE_LOADED = 2;
+	var READYSTATE_INTERACTIVE = 3;
+	var READYSTATE_COMPLETE = 4;
+
+	// jquery object
+	$castWrap = $('#cast_articles'),
+	SEARCH_DOCUMENT_URL = 'http://news.naver.com/main/search/search.nhn?query=';
+
+	function init() {
+		overrideNewsClickEvent();
+	}
+
+	function overrideNewsClickEvent() {
+		$castWrap.delegate('a', 'click', function(e) {
+			moveToEditedPage($(this));
+			e.preventDefault();
+
+		});
+	}
+
+	function moveToEditedPage($a) {
+		var title = getTitle($a);
+		console.log(title);
+
+		var xmlHttpObj = new XMLHttpRequest();
+
+		if (xmlHttpObj) {
+			// title EUC-KR 전환 필요.
+			var url = SEARCH_DOCUMENT_URL
+					+ title;
+			console.log(url);
+
+			xmlHttpObj.open("GET", url, true);
+			xmlHttpObj.onreadystatechange = function() {
+				if (xmlHttpObj.readyState == READYSTATE_COMPLETE) {
+					// console.log(xmlHttpObj.responseText);
+					var htmlTXT = xmlHttpObj.responseText;
+					var el = $(htmlTXT).find(".in_naver");
+					console.log(el);
+					if(el.length>0 && el[0].href!=null){
+						document.location.href = el[0].href;
+					}else{
+						alert("fail to search.");
+					}
+					return;
+				}
+			}
+			xmlHttpObj.send(null);
+		}
+
+	}
+
+	function getTitle($a) {
+		var title = $a.text() || $a.find('img').attr('alt'); // ?대?吏??
+																// ?ы븿??寃쎌슦 alt
+																// ?띿꽦??媛?졇?⑤떎.
+		// ?댁뒪 ?쒕퉬?ㅼ뿉?쒕뒗 怨듬갚??+濡???껜?쒕떎.
+		title = title.replace(/\s/g, '+');
+		return title;
+	}
+
+	init();
+
 }());
